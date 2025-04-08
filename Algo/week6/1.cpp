c2103028bb422465f24fb81f19cd5935a037f311
iui-whgi <stommy147@gmail.com>
2025-04-08 17:37:52 +0900

    6week 1번푸는중

---
+++ Algo/week6/1.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <vector>  
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+typedef vector<vector<int>> matrix;
+
+int main(){
+
+    int n;
+
+    cin >> n;
+
+    vector<int> a(n);
+
+    
+
+    matrix M(n);
+    matrix P(n);
+
+    for(int i=0; i<n;i++){
+        cin >> a[i];
+    }
+    
+    Chained( )
+
+
+    
+
+    
+}
